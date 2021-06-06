1f04b7277cab9a5c97db3c3f024942b0db890484
octavioduarte <octavioduarte2105@gmail.com>
2021-06-06 04:59:24 -0300

    create method to generete current date time and ID before save a new user on DB

--- domain/base.go
+++ domain/base.go
@@ -1,11 +1,35 @@
 package domain
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+)
 
 type Base struct {
-	ID        string
-	Active    bool
-	CreatedAt time.Timer
-	UpdatedAt time.Timer
-	DeletedAt time.Timer
+	ID        string    `json:"id" gorm:"type:uuid;primary_key"`
+	Active    bool      `json:"active" gorm:"type:boolean"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
+
+func (base *Base) BeforeCreate(scope *gorm.Scope) error {
+	err := scope.SetColumn("CreatedAt", time.Now())
+
+	if err != nil {
+		log.Fatalf("Error when set created_at on user model %v:", err)
+		return err
+	}
+
+	err = scope.SetColumn("ID", uuid.NewV4().String())
+
+	if err != nil {
+		log.Fatalf("Error when set ID on user model %v", err)
+		return err
+	}
+
+	return nil
 }
