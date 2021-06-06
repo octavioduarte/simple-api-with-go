3e8e0d7e62220c4d86074a9a5b2cfd7719a596ef
octavioduarte <octavioduarte2105@gmail.com>
2021-06-06 02:49:46 -0300

    create method to generate hash password and generate token to user

--- domain/user.go
+++ domain/user.go
@@ -1,9 +1,29 @@
 package domain
 
+import (
+	"log"
+
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	Base
 	Email    string
 	Name     string
 	Password string
 	Token    string
+}
+
+func (user *User) Prepare() error {
+	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		log.Fatalf("Error during the password generation: %v", err)
+	}
+
+	user.Password = string(password)
+	user.Token = uuid.NewV4().String()
+
+	return err
 }
